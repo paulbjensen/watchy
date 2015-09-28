9e4cfb6e253d476fc92382d24ad43d2b93f4d4a1
Paul Jensen <paulbjensen@gmail.com>
2015-09-28 08:52:01 +0100

    Added the ability to have new tweets appear as desktop notifications

--- app.js
+++ app.js
@@ -3,19 +3,25 @@
 
 var Twitter = require('twitter'); 
 var config	= require('./config');
-
+var term;
 var client = new Twitter(config);
 
 
+function notifyOfTweet (tweet) {
+	new window.Notification('New tweet about ' + term, {
+		icon: tweet.user.profile_image_url,
+		body: tweet.text
+	});
+} 
+
 function search () {
 	var formInput = window.document.querySelector('form input');
-	client.stream('statuses/filter', {track: formInput.value},  function (stream) {
-	  	stream.on('data', function(tweet) {
-		    console.log(tweet.text);
-	  	});
+	term = formInput.value;
+	client.stream('statuses/filter', {track: term},  function (stream) {
+	  	stream.on('data', notifyOfTweet);
 
-		stream.on('error', function(error) {
-		    console.log(error);
+		stream.on('error', function (error) {
+		    console.log(error.message);
 	  	});
 	});
 	return false;
